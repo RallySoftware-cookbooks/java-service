7de35aa3ae461c011e8ba6088bc8a1943197fd6a
Jeff Smith <jsmith@rallydev.com>
2013-10-29 09:54:27 -0600

    updating metadate to put version file in recipes

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'rallysoftware-cookbooks@rallydev.com'
 license          'MIT'
 description      'Installs/Configures java-service'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          IO.read(File.join(File.dirname(__FILE__), 'VERSION'))
+version          IO.read(File.join(File.dirname(__FILE__), 'recipes/VERSION')) rescue '0.1.0'
 
 # include all tested operating systems as part of the supports section
 # supports 'centos'
