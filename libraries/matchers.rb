f4e7741d00514165fb4aa7962e308510a41b4fe3
Russ Teabeault <rteabeault@rallydev.com>
2013-11-25 12:49:27 -0700

    Added matchers

---
+++ libraries/matchers.rb
@@ -0,0 +1,33 @@
+if defined?(ChefSpec)
+  def create_java_service(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:java_service, :create, resource_name)
+  end
+
+  def start_java_service(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:java_service, :start, resource_name)
+  end
+
+  def stop_java_service(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:java_service, :stop, resource_name)
+  end
+
+  def enable_java_service(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:java_service, :enable, resource_name)
+  end
+
+  def disable_java_service(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:java_service, :disable, resource_name)
+  end
+
+  def load_java_service(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:java_service, :load, resource_name)
+  end
+
+  def restart_java_service(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:java_service, :restart, resource_name)
+  end
+
+  def reload_java_service(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:java_service, :reload, resource_name)
+  end
+end
