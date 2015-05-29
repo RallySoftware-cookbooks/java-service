2f57b9ade85b0edb3093008b77ebfe8932def916
Russ Teabeault and Jeff Smith <rteabeault@rallydev.com>
2015-05-29 13:10:34 -0600

    Changing java_service to use rs_bluepill instead of bluepill
    - Forgot to fix unit test

--- test/unit/default_spec.rb
+++ test/unit/default_spec.rb
@@ -4,5 +4,5 @@ describe 'java_service::default' do
   subject { ChefSpec::Runner.new(log_level: :error).converge described_recipe }
 
   it { should include_recipe 'java' }
-  it { should include_recipe 'bluepill' }
+  it { should include_recipe 'rs_bluepill' }
 end
