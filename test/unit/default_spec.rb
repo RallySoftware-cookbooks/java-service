21606ccecdfd23da7ddc026dd356c213a43c730c
Ali Yahya <amyahya@gmail.com>
2017-05-18 20:52:16 -0400

    Update tests.

--- test/unit/default_spec.rb
+++ test/unit/default_spec.rb
@@ -1,7 +1,7 @@
 require_relative 'spec_helper'
 
 describe 'java_service::default' do
-  subject { ChefSpec::Runner.new(log_level: :error).converge described_recipe }
+  subject { ChefSpec::SoloRunner.new(platform: 'centos', version: '6.8', log_level: :error).converge described_recipe }
 
   it { should include_recipe 'java' }
   it { should include_recipe 'rs_bluepill' }
